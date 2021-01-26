d60c0b6570ae38d988299c6f22ffa7426684e5bc
s3829198 <58408410+s3829198@users.noreply.github.com>
2021-01-26 19:05:16 +1000

    Progress Update
    Add main content and fix page navigation.

--- a2/index.php
+++ a2/index.php
@@ -36,7 +36,7 @@ body {
                     <input type="text" placeholder="Mobile">
                     <input type="text" placeholder="Subject">
                     <input class="message" type="text" placeholder="Type your message here">
-                    <a href="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" class="button">Submit</a>
+                    <a href="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" class="button1">Submit</a>
                      <button id="closeButton" onclick="closeModal()">Close</button>
                     </form>
                     
@@ -49,13 +49,13 @@ body {
     <nav>
         <div class="navi">
         <ul>
-  <li><a href="index.php">Home</a></li>
+  <li><a class="active" href="#home">Home</a></li>
   <li><a href="introduction.php">Introduction</a></li>
-            <li><del><a href="">Letters &#38; Post Cards</a></del></li>
-            <li><del><a href="">Description of Battle Action</a></del></li>
-  <li><a class="active" href="#places">Places</a></li>
-            <li><del><a href="">Links to Related Material</a></del></li>  
-            <li><del><a href="">Comments</a></del></li>
+  <li><del><a href="">Letters &#38; Post Cards</a></del></li>
+  <li><del><a href="">Description of Battle Action</a></del></li>
+  <li><a href="places.php">Places</a></li>
+  <li><del><a href="">Links to Related Material</a></del></li>  
+  <li><del><a href="">Comments</a></del></li>
   <li><del><a href="">Sitemap</a></del></li>
 </ul>
         </div>
@@ -63,90 +63,13 @@ body {
     
     <main>
         <div class="info">
-            <h1 class="cont-title" >Places</h1>
-            <table> 
-<dl>
-    <dt><h1>A.</h1></dt>
-    <dd><h2>Abbeville, Aden, Ailly,Albany, Albert, Alexandria, Amiens, Anzac.</h2></dd>
-<br>
-    <dt><h1>B.</h1></dt>
-    <dd><h2>Baileul, Bapaume, Beauval, Bellancourt, Boescheppe, Bonneville, Buire.</h2></dd>
-<br>
-    <dt><h1>C.</h1></dt>
-    <dd><h2>Cairo, Cassel, Colombo (Sri-Lanka), Contalmaison, Croisy.</h2></dd>
-<br>
-    <dt><h1>D.</h1></dt>
-    <dd><h2>Dardenelles, Dernancourt, Doullens.</h2></dd>
-<br>
-    <dt><h1>E.</h1></dt>
-    <dd><h2>Egypt, Enoggera, Estaires.</h2></dd>
-<br>
-    <dt><h1>F.</h1></dt>
-    <dd><h2>Fleurs, Fort Ricasoli, Fort Tigne, Fricourt.</h2></dd>
-<br>
-    <dt><h1>G.</h1></dt>
-    <dd><h2>Gab-el-Habieta, Gallipoli, Godeswanvelde, Grantham.</h2></dd>
-<br>
-    <dt><h1>H.</h1></dt>
-    <dd><h2>Halleborouck, Halloy-Pernois, Harponville, Havre, Hazebrouck, Hedauville, Hellabouk, Helmich, Henencourt Wood, Herrincort, Hill 60.</h2></dd>
-<br>
-    <dt><h1>I.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>J.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>K.</h1></dt>
-    <dd><h2>Kasî-el-Nil Barracks.</h2></dd>
-<br>
-    <dt><h1>L.</h1></dt>
-    <dd><h2>La Boiselle, Le Panne, Lemnos Island, Liderzelle, London, Longueval.</h2></dd>
-<br>
-    <dt><h1>M.</h1></dt>
-    <dd><h2>Malta, Marseilles, Melaulte, Melbourne, Mena Camp.</h2></dd>
-<br>
-    <dt><h1>N.</h1></dt>
-    <dd><h2>Naours, Noordpiene.</h2></dd>
-<br>
-    <dt><h1>O.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>P.</h1></dt>
-    <dd><h2>Palmas, Pinkenba, Polincove, Pont-Remy, Poperinghe, Port Said, Pozieres, Proven.</h2></dd>
-<br>
-    <dt><h1>Q.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>R.</h1></dt>
-    <dd><h2>Reninghelst, Rubempre.</h2></dd>
-<br>
-    <dt><h1>S.</h1></dt>
-    <dd><h2>Sailley, Serapeum, Slienna, Somme, Southampton, Steenvoorde, Strazelle, Suez, Sutton.</h2></dd>
-<br>
-    <dt><h1>T.</h1></dt>
-    <dd><h2>Tel-el-Kebir, Toutencourt, Trones.</h2></dd>
-<br>
-    <dt><h1>U.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>V.</h1></dt>
-    <dd><h2>Valetta, Varrenes.</h2></dd>
-<br>
-    <dt><h1>W.</h1></dt>
-    <dd><h2>Wood, W.A., Watten.</h2></dd>
-<br>
-    <dt><h1>X.</h1></dt>
-    <dd style="color:red"><h2>No mentions yet.</h2></dd>
-<br>
-    <dt><h1>Y.</h1></dt>
-    <dd><h2>Ypres.</h2></dd>
-<br>
-   <dt><h1>Z.</h1></dt>
-    <dd><h2>Zeitoun.</h2></dd>
-<br>
-    
-</dl>
-</table></div>
+            <h1 class="cont-title" >Home</h1>
+            <h3>Hello and welcome,</h3>
+            <h3>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and inparticular, those who created the ANZAC legend.</h3>
+            <h3>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.<br><img src="../../media/D.R.%20Baker%20F.Co%20one%20of%20the%20soldiers%20photographed%20in%20The%20Queenslander%20Pictorial%20supplement%20to%20The%20Queenslander%201914..jpg" class="Baker" alt="D.R. Baker" width="130" height="200"><br>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.<br><br>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.<br><br>From the menu on the left, read the Introduction to set the scene. Then, to start reading the letters, click on Letter and Post Cards in the menu on the left. All the letters are searchable using the  search bar at the top right of this page.</h3>
+            <br>
+            <h5><i>(Photograph courtesy of : John Oxley Library, State Library of Queensland [Image number: 702692-19141024-s0023-0027])</i></h5>
+            </div>
         </main>
         
     <footer>
--- a2/introduction.php
+++ a2/introduction.php
@@ -1,1 +1,129 @@
-<p>Introduction</p>
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="A remodeled version of Douglas Ramsey Bakers war tales.">
+    <link rel="stylesheet" href="style2.css">
+    <title>ANZAC Douglas Raymond Baker</title>
+    
+    <style> 
+body {
+  background-image:url(../../media/background.jpg); 
+    object-fit: fill;
+}
+</style>
+    
+    </head>
+    
+    <header>
+        <script src='A2Script.js'></script>
+        <div class="left">
+        <p>ANZAC Douglas Raymond Baker - Letters Home</p>
+        </div>
+            <div class="right">
+                <br>
+                <div class="contButton">
+                <button class="button" id="button" onclick="openModal()">Contact Us Available Here</button>
+                </div>
+                <div class="bg-modal">
+                <div class="modal-content">
+                    <p class="contact-title">Contact Us<p>
+                    <form action="">
+                    <input type="text" placeholder="Name">
+                    <input type="text" placeholder="Email">
+                    <input type="text" placeholder="Mobile">
+                    <input type="text" placeholder="Mobile">
+                    <input type="text" placeholder="Subject">
+                    <input class="message" type="text" placeholder="Type your message here">
+                    <a href="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" class="button">Submit</a>
+                     <button id="closeButton" onclick="closeModal()">Close</button>
+                    </form>
+                    
+                    </div>
+                </div>
+                </div>
+        
+        </header>
+        
+    <nav>
+        <div class="navi">
+        <ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a class="active" href="#introduction">Introduction</a></li>
+  <li><del><a href="">Letters &#38; Post Cards</a></del></li>
+  <li><del><a href="">Description of Battle Action</a></del></li>
+  <li><a href="places.php">Places</a></li>
+  <li><del><a href="">Links to Related Material</a></del></li>  
+  <li><del><a href="">Comments</a></del></li>
+  <li><del><a href="">Sitemap</a></del></li>
+</ul>
+        </div>
+        </nav>
+    
+    <main>
+        <div class="info">
+            <h1 class="cont-title" >Introduction</h1>
+            <h2><u>By Douglas Richard Baker</u></h2>
+            <h3>(Son of Douglas Raymond Baker)</h3>
+            <br>
+            <h3>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.</h3>
+            <br>
+            <h3>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.</h3>
+            <br>
+            <h3>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.</h3>
+            <br>
+            <h3>Where Dad used brackets in a letter I have used [   ]  style and where I have made any comment or explanation I have used (   ) and Italics.</h3>
+            <br>
+            <h3>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the original meaning of the word before it became associated with the homosexual community.</h3>
+            <br>
+            <h3>Some abbreviations and terms:-</h3>
+            <table>
+            <dl>
+                <dd><h3>Coy = Company(part of a Battalion)</h3></dd>
+                <dd><h3>Batt. = Battalion</h3></dd>
+                <dd><h3>Col. = Colonel</h3></dd>
+                <dd><h3>Sergt. = Sergeant</h3></dd>
+                <dd><h3>Capt. = Captain</h3></dd>
+                <dd><h3>Lieut. = Lieutenant (pronounced “Lef-tenant”)</h3></dd>
+                <dd><h3>Q.M. = Quarter Master</h3></dd>
+                <dd><h3>Comp. = Company</h3></dd>
+                <dd><h3>PC = Post Card (not Personal Computer!)</h3></dd>
+                <dd><h3>CB = Confined to Barracks (leave stopped as a punishment)</h3></dd>
+                <dd><h3>Big hits = top ranked officers</h3></dd>
+                <dd><h3>Brass hats = top ranked officers</h3></dd>
+                <dd><h3>Dry canteen = bar and/or cafeteria, etc. where  no alcohol is served.</h3></dd>
+                <dd><h3>Wet canteen = bar and/or cafeteria, etc. where alcohol  is available.</h3></dd>
+                <dd><h3>Sapping  =  digging a tunnel or deep trench to approach or undermine an enemy position.</h3></dd>
+                <dd><h3>L. Horse = Light Horse</h3></dd>
+                <dd><h3>Good nick or great nick = good health</h3></dd>
+                <dd><h3>Bonsorful = wonderful, marvellous (in 2009speak, “awesome”, “cool”.)</h3></dd>
+                <dd><h3> M.G.S. = Machine Gun Section</h3></dd>
+                <dd><h3>Territorials = soldiers of the English Volunteer Reserve</h3></dd>
+                <dd><h3>Terriers = Territorials</h3></dd>
+                <dd><h3>Bomb = unless dropped from an aeroplane these would have been Mills Bombs – an early version of the hand grenade.</h3></dd>
+                <dd><h3>Blighty = England</h3></dd>
+                <dd><h3>Duke = Dad's/family's dog</h3></dd>
+                <dd><h3>O.R. = Orderly Room</h3></dd>
+            </dl>
+            </table>
+            <br>
+            <h2><u>Additional Comments by Ian Stuart Baker</u></h2>
+            <h3>(Son of Douglas Richard Baker)</h3>
+            <br>
+            <h3>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</h3>
+            <br>
+            <h3>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.</h3>
+            <h3>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</h3>
+            </div>
+        
+        </main>
+        
+    <footer>
+        <div class="foot">
+            <h3> © Ian Baker - ibak6837@bigpond.net.au || Joshua Lishman 2021 https://github.com/s3829198/wp.git </h3>
+        </div>
+        </footer> 
+
+</html>
+ <!-- Wallpaper Flare, n.d. Parchment Background Image. [image] Available at: <https://www.wallpaperflare.com/abstract-ancient-antique-art-background-canvas-close-up-wallpaper-prqcu> [Accessed 17 January 2021]. -->
