dfaf6ed978ba41a07a5f078f27bcadb49341d055
s3829198 <58408410+s3829198@users.noreply.github.com>
2021-01-26 11:49:39 +1000

    Add Major Update To A2

---
+++ a2/A2Script.js
@@ -0,0 +1,10 @@
+function openModal() {
+     document.getElementById('button').addEventListener('click', function() {
+        document.querySelector('.bg-modal').style.display = 'flex'; 
+     });
+}
+
+function closeModal() {
+    document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('bg.modal').style.display = 'none'; });
+}
